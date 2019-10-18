8a77e4964ea64d221918bda6fb6d711abd4b7258
Charles Xu <charl3s.xu@gmail.com>
2019-10-18 23:52:40 +0800

    .

--- MATLAB/__init__.py
+++ MATLAB/__init__.py
@@ -1,2 +1,3 @@
 from .num import mat, inv, rank, det, exp
 from .sym import smat, sinv, sdet, sexp, syms
+from .plot import fig3d, surf, grid
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ readme = '\n'.join(open('README.md').readlines())
 
 setuptools.setup(
     name='pyMATLABstyle',
-    version='1.1.0',
+    version='1.1.1',
 
     # Project description
     description='MATLAB style of creating numpy/sympy matrices, and a few MATLAB style functions',
